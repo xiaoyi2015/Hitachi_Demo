c8d6b45cd4dd0f93ebb9eaa604112cde8e2c5b1e
unknown <zln19921108@126.com>
2015-09-18 11:19:51 +0800

    user_information page

--- app/src/main/java/ac/airconditionsuit/app/activity/UserInfoActivity.java
+++ app/src/main/java/ac/airconditionsuit/app/activity/UserInfoActivity.java
@@ -1,7 +1,24 @@
 package ac.airconditionsuit.app.activity;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import ac.airconditionsuit.app.R;
+
 /**
  * Created by Administrator on 2015/9/18.
  */
-public class UserInfoActivity {
+public class UserInfoActivity extends FragmentActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_information);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
