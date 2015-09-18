137f09d2a1306811198c725b0c5f3fc8987cbd3f
unknown <zln19921108@126.com>
2015-09-18 10:27:09 +0800

    page jump

---
+++ app/src/main/java/ac/airconditionsuit/app/activity/SoftwareInfoActivity.java
@@ -0,0 +1,24 @@
+package ac.airconditionsuit.app.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import ac.airconditionsuit.app.R;
+
+/**
+ * Created by Administrator on 2015/9/18.
+ */
+public class SoftwareInfoActivity extends FragmentActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.software_info);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
