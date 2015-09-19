310baf17d54458cc10096ff975fc36cf3fc0743d
unknown <zln19921108@126.com>
2015-09-19 10:34:18 +0800

    merge

--- app/src/main/java/ac/airconditionsuit/app/activity/BaseActivity.java
+++ app/src/main/java/ac/airconditionsuit/app/activity/BaseActivity.java
@@ -1,5 +1,6 @@
 package ac.airconditionsuit.app.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -11,6 +12,7 @@ import ac.airconditionsuit.app.view.CommonTopBar;
  */
 public class BaseActivity extends FragmentActivity {
 
+    private static final String ACTIVITY_FROM = "activity_from" ;
     //Log 信息的时候使用的tag，尽量不要用system.out
     static protected String TAG;
     {
@@ -27,6 +29,16 @@ public class BaseActivity extends FragmentActivity {
 
     public CommonTopBar getCommonTopBar() {
         return commonTopBar;
+    }
+
+    public void shortStartActivity(Class c, String... keyAndValue) {
+        Intent intent = new Intent(this, c);
+        int keyAndValueLength = keyAndValue.length;
+        for (int i = 0; i < keyAndValueLength / 2; ++i) {
+            intent.putExtra(keyAndValue[i * 2], keyAndValue[i * 2 + 1]);
+        }
+        intent.putExtra(ACTIVITY_FROM, this.getClass().getName());
+        startActivity(intent);
     }
 
 }
