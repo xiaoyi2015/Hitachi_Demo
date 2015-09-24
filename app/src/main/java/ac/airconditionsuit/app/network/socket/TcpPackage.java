9db8900e4d3627baaf1769aa2dde0c6e798b1f99
albuscrow <albuscrow@gmail.com>
2015-09-24 20:50:15 +0800

    add some code for socket

--- app/src/main/java/ac/airconditionsuit/app/network/socket/TcpPackage.java
+++ app/src/main/java/ac/airconditionsuit/app/network/socket/TcpPackage.java
@@ -3,7 +3,9 @@ package ac.airconditionsuit.app.network.socket;
 /**
  * Created by ac on 9/24/15.
  */
-public class SocketPackage {
+public class TcpPackage extends SocketPackage{
+
+    @Override
     byte[] getBytes() {
         return new byte[0];
     }
