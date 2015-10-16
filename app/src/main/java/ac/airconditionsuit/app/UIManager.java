95556bd19389d7f6fb57f1a9b571f52b93364769
unknown <zln19921108@126.com>
2015-10-16 10:53:16 +0800

    change the login xml to hit

--- app/src/main/java/ac/airconditionsuit/app/UIManager.java
+++ app/src/main/java/ac/airconditionsuit/app/UIManager.java
@@ -4,18 +4,18 @@ package ac.airconditionsuit.app;
  * Created by ac on 10/15/15.
  */
 public class UIManager {
-    public static final int RILI = 1;
-    public static final int HAIXIN = 2;
-    public static final int D_CONTROL = 3;
+    public static final int HIT = 1;
+    public static final int HX = 2;
+    public static final int DC = 3;
 
-    public static final int UITYPE = RILI;
+    public static final int UITYPE = HIT;
 
-    int getRoomLayout() {
+    public static int getLoginLayout() {
         switch (UITYPE) {
-            case RILI:
-                return 0;
-            case HAIXIN:
-                return 0;
+            case HIT:
+                return R.layout.activity_login_hit;
+            case DC:
+                return R.layout.activity_login;
             default:
                 return 0;
         }
