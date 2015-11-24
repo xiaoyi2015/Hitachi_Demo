98c768a0bd618c463947a227bc638b7b789a005d
unknown <zln19921108@126.com>
2015-11-24 13:18:40 +0800

    fix warning icon and test ok

--- app/src/main/java/ac/airconditionsuit/app/activity/RoomAirSettingHitActivity.java
+++ app/src/main/java/ac/airconditionsuit/app/activity/RoomAirSettingHitActivity.java
@@ -12,6 +12,7 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -316,23 +317,23 @@ public class RoomAirSettingHitActivity extends BaseActivity {
 
                         TextView et = new TextView(RoomAirSettingHitActivity.this);
                         et.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-                        et.setGravity(Gravity.LEFT|Gravity.CENTER);
+                        et.setGravity(Gravity.LEFT|Gravity.CENTER_VERTICAL);
                         String warning = " \n";
                         for (int i = 0; i < air_index_list.size(); i++) {
                             if (warning_list.get(i) == -2) {
                                 if (address_list.get(i) < 10) {
-                                    warning = warning + "空调 " + air_index_list.get(i) + "-0" +
+                                    warning = warning + "          空调 " + air_index_list.get(i) + "-0" +
                                             address_list.get(i) + "离线" + "\n";
                                 } else {
-                                    warning = warning + "空调 " + air_index_list.get(i) + "-" +
+                                    warning = warning + "          空调 " + air_index_list.get(i) + "-" +
                                             address_list.get(i) + "离线" + "\n";
                                 }
                             } else {
                                 if (address_list.get(i) < 10) {
-                                    warning = warning + "空调 " + air_index_list.get(i) + "-0" +
+                                    warning = warning + "          空调 " + air_index_list.get(i) + "-0" +
                                             address_list.get(i) + "，报警代码：" + Integer.toHexString(warning_list.get(i)| 0xFFFFFF00).substring(6) + "\n";
                                 } else {
-                                    warning = warning + "空调 " + air_index_list.get(i) + "-" +
+                                    warning = warning + "          空调 " + air_index_list.get(i) + "-" +
                                             address_list.get(i) + "，报警代码：" +  Integer.toHexString(warning_list.get(i)| 0xFFFFFF00).substring(6) + "\n";
                                 }
                             }
